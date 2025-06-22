2b33aa1e55bd9447dd66d4bd4d2fc11bdb562aba
NK <smnk26@protonmail.com>
2025-06-22 17:08:05 -0500

    Chat bot

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,20 @@
+import { useEffect } from "react";
 import Footer from "./Footer";
 import ScrollToTopButton from "./ScrollToTopButton";
 
-
 function Layout({ children }) {
+  useEffect(() => {
+    
+    const script = document.createElement("script");
+    script.src = "//code.tidio.co/0qlw0mvrortlsrtbox0lg8sgjmywtqiv.js"; 
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <div className="bg-gray-900 min-h-screen text-gray-100 font-montserrat px-6 py-12 flex flex-col justify-between">
       <div className="flex-grow">{children}</div>
+
       <ScrollToTopButton />
       <Footer />
     </div>
@@ -13,6 +22,7 @@ function Layout({ children }) {
 }
 
 export default Layout;
+
 
 
 
